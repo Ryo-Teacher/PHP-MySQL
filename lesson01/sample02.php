2d316cc12bcedba897faf6443a97c65116271502
github Ryo-Teacher <ryo_aside_agent@yahoo.co.jp>
2024-08-21 23:11:25 +0900

    second commit
    エスケープシーケンスを学びました。

---
+++ lesson01/sample02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sample02</title>
+</head>
+<body>
+  <?php
+    echo 'I\'m studying';
+    // エスケープシーケンスとは、シングルクオーテーション（’）やダブルクオーテーション（”）内で同じ記号を使いたい時に、バックスラッシュ（\）を前に入れることで、無効化させること。
+    // ※バックスラッシュの出し方はoptionキー+¥マーク
+    // エスケープシーケンスは他にもいくつかの種類がある（詳しくはリファレンス参照）。
+  ?>
+</body>
+</html>

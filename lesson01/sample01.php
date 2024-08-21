67957ca3fe4564c20c3d01d0c9995e434009345f
github Ryo-Teacher <ryo_aside_agent@yahoo.co.jp>
2024-08-21 22:38:52 +0900

    intial commit

---
+++ lesson01/sample01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sample01</title>
+</head>
+<body>
+  <?php
+    echo 'PHPを勉強中です';
+    // echo → 出力する。画面に表示する。ファンクション
+    // ' 〜 ' → パラメーター。（）で囲うパラメーターもあるが、echoは囲わない。
+    ?>
+</body>
+</html>
